df26f4449fe94255bae3fc2b2c2c6680243ddd07
Rehan Islam <2003rehan@gmail.com>
2025-06-06 17:46:34 -0400

    added port

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const connection = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  port: 3306
 });
 
 connection.connect((err) => {
