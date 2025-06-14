78309e7a6729cb20bd52abfff930959553fb7c09
lolaquraishi <uev7gx@virginia.edu>
2025-06-13 20:06:08 -0400

    removed volunteer sign up and commitee

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -26,11 +26,11 @@ const NavBar = () => {
         <ul className="nav-list">
           <li><Link to="/home">Home</Link></li>
           <li><Link to="/classcouncil">Class Council</Link></li>
-          <li><Link to="/committees">Committees</Link></li>
+          {/* <li><Link to="/committees">Committees</Link></li> */}
           <li><Link to="/events">Events</Link></li>
           <li><Link to="/advisors">Advisors</Link></li>
           {/* <li><Link to="/budget">Budget</Link></li> */}
-          <li><Link to="/volunteersignup">Volunteer Sign Up</Link></li>
+          {/* <li><Link to="/volunteersignup">Volunteer Sign Up</Link></li> */}
         </ul>
       </div>
 
